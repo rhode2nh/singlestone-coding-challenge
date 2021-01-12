77308c445819866d794d98697187bfd4748e129a
rhode2nh <rhode2nh@protonmail.com>
2021-01-12 18:59:28 -0500

    retreived data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react'
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    fetch('https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge')
+      .then(resp => resp.json())
+      .then(data => sanitizeData(data))
+      .catch(error => console.log(error))
+  }, [])
+
+  const sanitizeData = (steps) => {
+    console.log(steps)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h2>test</h2>
+  )
 }
 
 export default App;
